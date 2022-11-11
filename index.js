d2434d09e89ad38f6e6e2384402a8f3e0d377471
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2022-11-11 18:03:13 +0000

    icelandic letters

---
+++ index.js
@@ -0,0 +1,23 @@
+import express, { application } from 'express';
+import men from './js/males.js';
+import women from './js/females.js';
+import checkName from './js/main.js';
+const app = express();
+
+app.get('/', (req, res) => {
+  const firstnames = req.query.name.split(" ");
+  let nameList = [...men.results, ...women.results];
+  /*if (req.query.gender === 'female') {
+    nameList = women.results;
+  }
+  else{
+    nameList = men.results;
+  }*/
+  let results1 = "";
+  for (var i = 0; i < firstnames.length; i++) {
+    results1 += checkName(firstnames[i], nameList) + " ";
+  }
+  res.json(results1);
+})
+
+app.listen(3000);
---
+++ js/main.js
@@ -0,0 +1,51 @@
+var checkName=function(name, names){
+  const capitalizeFirstLetter = (n)=> n.charAt(0).toUpperCase() + n.slice(1);
+  name = name.toLowerCase();
+  console.log("upper name is: " + name);
+  const map = {'a': 'á', 'd': 'ð', 'e': 'é', 'i': 'í', 'o': 'ó', 'u': 'ú', 'y': 'ý'};
+	
+
+  for (let i=1; i<name.length; i++){
+    //change th to þ  
+    if(name[i-1]=='t' && name[i]=='h'){
+      name = name.slice(0,i-1)+'þ'+ name.slice(i+1,name.length)
+      //name = 'þ' + name.slice(i-1,2,name.length);
+    }
+    //change ae to æ
+    if(name[i-1]=='a' && name[i]=='e'){
+      name = name.slice(0,i-1)+'æ'+ name.slice(i+1,name.length)
+      
+      //name = 'æ' + name.slice(2,name.length);
+    }
+    //change oe to ö
+    if(name[i-1]=='o' && name[i]=='e'){
+      name = name.slice(0,i-1)+'ö'+ name.slice(i+1,name.length)
+      
+      //name = 'ö' + name.slice(2,name.length);
+    }
+  }
+
+  const possibleNames = [name];
+  for(let c in name){
+    
+    const replacement = map[name[c]] //will be undefined if the letter does not have a corresponding special character
+    if(replacement) {
+      possibleNames.forEach(n=>{
+        possibleNames.push(n.slice(0,c)+replacement+ n.slice(Number(c)+1,n.length))
+      })
+      //need to push to new array for each possibleWrongCharacter a new version of the word
+
+    }
+  }
+  let IceName = ""
+  console.log(possibleNames)
+	
+  possibleNames.forEach(n=>{
+    const N = capitalizeFirstLetter(n)
+    if (names.includes(N)) IceName = N
+  })
+	
+  return IceName;
+}
+
+export default checkName;
